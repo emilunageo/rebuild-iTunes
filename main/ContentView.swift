c0d2ebfa6da94de7c44878eab60f946dc465a339
Emiliano Luna <emiliano.george@gmail.com>
2025-11-07 12:16:53 +0100

    First commit

---
+++ main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  main
+//
+//  Created by Emiliano Luna George on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
